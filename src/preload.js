07a45ebcf983d9559efe51a9746b8eaab64e92ef
cybersecurity <inboxsentrylabs@gmail.com>
2025-05-23 20:08:51 -0400

    Update preload.js
    Signed-off-by: cybersecurity <inboxsentrylabs@gmail.com>

--- src/preload.js
+++ src/preload.js
@@ -3,3 +3,9 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electronAPI', {
     scanPC: () => ipcRenderer.invoke('scan-pc')
 });
+contextBridge.exposeInMainWorld('api', {
+  twoFactorAuth: {
+    generateSecret: (userId) => ipcRenderer.invoke('2fa:generate-secret', userId),
+    verifyCode: (data) => ipcRenderer.invoke('2fa:verify-code', data)
+  }
+});
